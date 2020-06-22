59672e81c54b30d6928a5bf462458a1b16772c75
Oliver Lorenz <mail@oliverlorenz.com>
2020-06-22 11:45:48 +0200

    fix: bump versions

--- src/postinstall.js
+++ src/postinstall.js
@@ -1,25 +1,19 @@
 const base = require('@high-standards-js/base');
 
-(async() => {
-    await base.checkAcceptedHighStandards();
+(async () => {
+  await base.checkAcceptedHighStandards();
 
-    let packageJsonOfConfig = base.getInitiatingProjectPackageJson();
-    
-    packageJsonOfConfig = await base.addDevDependency(packageJsonOfConfig, 'lint-staged');
-    packageJsonOfConfig = await base.addDevDependency(packageJsonOfConfig, 'prettier');
+  let packageJsonOfConfig = base.getInitiatingProjectPackageJson();
 
-    packageJsonOfConfig.husky.hooks['pre-commit'] = 'lint-staged';
-    if (!packageJsonOfConfig['lint-staged']) {
-        packageJsonOfConfig['lint-staged'] = {
-            '*.{ts,js,json,css,yaml}': [
-                'prettier --write',
-                'git add'
-            ]
-        }
-    }
-    base.writeFile(
-        '.prettierrc',
-        base.getTemplate(__dirname, '.prettierrc')
-    );
-    base.writeInitiatingProjectPackageJson(packageJsonOfConfig);
-})()
+  packageJsonOfConfig = await base.addDevDependency(packageJsonOfConfig, 'lint-staged');
+  packageJsonOfConfig = await base.addDevDependency(packageJsonOfConfig, 'prettier');
+
+  packageJsonOfConfig.husky.hooks['pre-commit'] = 'lint-staged';
+  if (!packageJsonOfConfig['lint-staged']) {
+    packageJsonOfConfig['lint-staged'] = {
+      '*.{ts,js,json,css,yaml}': ['prettier --write', 'git add'],
+    };
+  }
+  base.copyFileFromTemplate(__dirname, '.prettierrc');
+  base.writeInitiatingProjectPackageJson(packageJsonOfConfig);
+})();
